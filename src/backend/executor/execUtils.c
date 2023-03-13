e63bd2c4da02dc15fecec7e7b8f25597d4b1270a
amitlan <amitlangote09@gmail.com>
2023-03-13 15:59:38 +0900

    Track opened range table relations in a List in EState
    This makes ExecCloseRangeTableRelations faster when there are many
    relations in the range table but only a few are opened during
    execution, such as when run-time pruning kicks in on an Append
    containing 1000s of partition subplans.

--- src/backend/executor/execUtils.c
+++ src/backend/executor/execUtils.c
@@ -829,6 +829,8 @@ ExecGetRangeTableRelation(EState *estate, Index rti)
 		}
 
 		estate->es_relations[rti - 1] = rel;
+		estate->es_opened_relations = lappend(estate->es_opened_relations,
+											  rel);
 	}
 
 	return rel;
